eec966803a9d9043ca2af8a4529ccf8c992e07e2
Suman Das <40906107+0001SID@users.noreply.github.com>
2018-11-17 09:54:41 +0530

    Create sum.py

---
+++ old/sum.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 17 12:05:25 2018
+
+@author: SID
+"""
+
+x = int (input( 'Enter your number: '))
+y = int (input( 'Enter your second number: '))
+z = x+y
+print ( 'Summation of the two numbers is: ' + str (z))
