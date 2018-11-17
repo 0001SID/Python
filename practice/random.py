84d12146ee2773d48772eb9ae7fb89de36e09cf1
0001SID <mailofsumandas@gmail.com>
2018-11-17 12:00:11 +0530

    initial

---
+++ practice/random.py
@@ -0,0 +1,5 @@
+import random
+print ("hello world")
+for i in range (5):
+    #print(i)
+    print(random.randint(1,10))
---
+++ practice/toss.py
@@ -0,0 +1,12 @@
+import random
+head = 0
+tail = 0
+total = 900000
+for i in range (total):
+    toss = random.randint(1,2)
+    if(toss == 1):
+        head += 1
+    else:
+        tail += 1
+print("head = ",head,"Tail = ",tail)
+print("Head percent = ",round((head*100)/total,2),"tail percent = ",round((tail*100)/total,2))
