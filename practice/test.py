37a38c4fd7e0ff8ce7ed922a4c26be0c057a04ec
0001SID <mailofsumandas@gmail.com>
2018-11-18 12:01:24 +0530

    update

--- practice/test.py
+++ practice/test.py
@@ -1,5 +1,9 @@
+import sys,os
 thing = ['good','bad']
 test = 'Atlast i get it'
 typ = [test for i in thing if i == 'good']
 for f in typ:
     print(f)
+    os._exit(0)
+if 's'=='s'=='c':
+    print('trye')
